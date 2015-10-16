c54edcd017da997907c61267ef50dfee974d8536
grammatiste <grammatiste8@gmail.com>
2015-10-16 10:28:55 -0400

    Créé: Fichier accueil.php. Structure html et lien à la page historique.php codée au complet.

---
+++ accueil.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="fr-CA">
+	<head>
+		<meta charset="utf-8" />
+		<title>Bienvenue sur notre site</title>
+	</head>
+	<body>
+		<header>
+			<h1></h1>
+		</header>
+
+		<nav>
+			<ul>
+				<li>
+					<a href="infos_initiales.php"></a>
+				</li>
+
+			</ul>
+		</nav>
+
+		<section class="section_top">
+			<article class="article_1">
+				<p>
+					Article 1
+				</p>
+			</article>
+
+		</section>
+
+		<section class="section_middle">
+			<article class="article_2">
+				<p>
+					Article 2
+				</p>
+			</article>
+		</section>
+		
+		<aside class="aside_right">
+			<img class="img_aside_right" src="img/logo.png" alt="Logo de l'entreprise">
+		</aside>
+		
+		<footer class="footer">
+		</footer>
+	</body>
+</html>
