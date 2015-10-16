a3e386c7fbfe7a9df2782214fbd76489c3a3caa6
grammatiste <grammatiste8@gmail.com>
2015-10-16 12:44:18 -0400

    Modifié: Ajouté structure html codé.

--- historique.php
+++ historique.php
@@ -1,1 +1,31 @@
 1. Ce code intégrera html5, javascript et JQuery, CSS3 et Bootstrap de même que le php requis pour le traitement des données sur le serveur.
+2. <!DOCTYPE html>
+	<html lang="fr-CA">
+		<head>
+			<meta charset="utf-8" />
+			<title>
+				Historique
+			</title>
+
+			<link href="" />
+		</head>
+
+		<body>
+			<header>
+				<h1>
+				</h1>
+			</header>
+			
+			<nav>
+			</nav>
+
+			<section class="section_top">
+			</section>
+
+			<footer>
+			</footer>
+
+			<script>
+			</script>
+		</body>
+	</html>
